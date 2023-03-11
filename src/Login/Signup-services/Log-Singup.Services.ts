87bc6e5806ac2a1b7aec6ea50a5281e7f23a03f1
Ankit <ankisharma864@gmail.com>
2023-03-11 16:34:04 +0530

    implementing route guards

--- src/Login/Signup-services/Log-Singup.Services.ts
+++ src/Login/Signup-services/Log-Singup.Services.ts
@@ -1,63 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import  {HttpClient, HttpClientModule, HttpParams ,HttpHeaders}  from '@angular/common/http'
-import { map, Observable } from 'rxjs';
+import { LoginModel, AuthenticatedResponse } from 'src/Interface/login-model';
+import { Router  } from '@angular/router';
+
+
+
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginSignUpApiService {
-  url = 'https://localhost:44354/Register'
-  endPoints ="/Register" 
+export class LoginSignUpApiService implements OnInit  {
+
+  invalidLogin: boolean = false;
+  credentials: LoginModel = {username:'', password:''};
+
+  url = 'https://localhost:44354'
+ 
+  endPoints = {
+    register:'/Register',
+    login:"/login"
+  }
 
   
 
-  constructor(private httpclient:HttpClient) {
+  constructor(private httpclient:HttpClient, private router:Router) {
    
    }
   
-  //  saveContactDetails() {
-  //   this.ngxLoader.start();
-  //   let user: any = {}
-  //   user.firstName = this.contactForm.value.firstName
-  //   user.lastName = this.contactForm.value.lastName
-  //   user.company = this.contactForm.value.company
-  //   user.dateOfBirth = this.contactForm.value.dateOfBirth
-  //   user.company = this.contactForm.value.company
-  //   user.email = this.contactForm.value.email
-  //   user.gender = this.contactForm.value.gender
-  //   user.title = this.contactForm.value.title
-  //   user.phone = this.contactForm.value.phone
-  //   user.contactId = this.selectedConact.contactId
-  //   user.updatedBy = this.userId
-  //   user.phoneNumber = this.contactForm.value.phoneNumber
-  //   user.groupId = this.contactGroup.value.group
-
-  //   if (this.isEdit) {
-  //     this.selectedConact.groupId > 0
-  //   }
-  //   this.contactService.saveContacts(user)
-  //     .then(res => {
-  //       this.ngxLoader.stop();
-  //       this.toastr.success(ResponseMessage.ContactSaved, MessageType.Success, { timeOut: 3000 })
-  //     }).catch(err => {
-  //       this.ngxLoader.stop();
-  //       this.toastr.error(ResponseMessage.Failed, MessageType.Error, { timeOut: 3000 })
-  //     })
-  // }
-
-  // saveContacts(data: any): Promise<any> {
-  //   let method = apiEndpoints.addContact;
-  //   return new Promise((resolve, reject) => {
-  //     this._httpClient.post(method, data)
-  //       .subscribe((res: any) => {
-  //         resolve(res)
-  //       }, reject)
-  //   })
-  // }
+ 
+   ngOnInit(): void {
+     
+   }
 
 
-   saveUser(data:any):Promise<any>{
+   saveUser(data:any) {
      debugger;
      const httpOptions = {
       headers: new HttpHeaders({
@@ -70,13 +47,29 @@ export class LoginSignUpApiService {
       Email:data.Email,
       Password:data.Password
     };
-        return  new Promise((resolve , reject) =>{ this.httpclient.post(this.url, 
-          payload,
-          httpOptions
-        ).subscribe((res:any)=>{
-          resolve(res)
-        }, reject)
-    })
+    return this.httpclient.post(this.url + this.endPoints.register , payload);
+    //     return  new Promise((resolve , reject) =>{ this.httpclient.post(this.url+this.endPoints.register, 
+    //       payload,
+    //       httpOptions
+    //     ).subscribe((res:any)=>{
+    //       resolve(res)
+    //     }, reject)
+    // })
+  }
+
+  login(data:any){
+    this.httpclient.post<AuthenticatedResponse>(this.url + this.endPoints.login, data, {
+        headers: new HttpHeaders({ "Content-Type": "application/json"})
+      }).subscribe({
+        next: (response: AuthenticatedResponse) => {
+          const token = response.token;
+          localStorage.setItem("jwt", token); 
+          this.invalidLogin = false; 
+          this.router.navigate(["/"]);
+        }
+        // error: (err: HttpErrorResponse) => this.invalidLogin = true
+        
+      })
   }
   //   const endPoints="/Register"  
   
